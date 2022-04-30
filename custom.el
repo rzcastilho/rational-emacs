4304603efc82214e2fa9fee114dbccf3838f4315
rzcastilho <rodrigo.zampieri@gmail.com>
2022-04-30 00:23:11 -0300

    Initial commit

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(gnu-elpa-keyring-update yasnippet-snippets vertico pkg-info org-tree-slide org-appear orderless mix marginalia keycast helpful exec-path-from-shell evil-nerd-commenter embark elixir-yasnippets elixir-mode elisp-demos eglot doom-themes doom-modeline dockerfile-mode docker-compose-mode dap-mode consult company))
+ '(rational-ui-default-font '(:font "JetBrains Mono" :weight light :height 120)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
